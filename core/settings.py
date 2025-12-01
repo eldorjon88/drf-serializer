0e5ff8f32a1365eeb08f7f933007308c238bb395
djumanov <djumanovdev@gmail.com>
2025-12-01 16:48:48 +0500

    restframework int

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+    'rest_framework',
+
+    # local apps
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
